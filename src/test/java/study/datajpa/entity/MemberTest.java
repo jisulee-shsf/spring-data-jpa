bee28927e5484582599e652d40f6d98e5162e49e
Jisu Lee <jisulee.shsf@gmail.com>
2024-10-08 22:37:00 +0900

    확장 기능 - 순수 JPA Auditing

--- src/test/java/study/datajpa/entity/MemberTest.java
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -3,9 +3,11 @@ package study.datajpa.entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import java.util.List;
 
@@ -16,6 +18,8 @@ class MemberTest {
 
     @PersistenceContext
     private EntityManager em;
+
+    @Autowired MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -46,5 +50,21 @@ class MemberTest {
             System.out.print("member = " + member);
             System.out.println(" -> member.getTeam() = " + member.getTeam());
         }
+    }
+
+    @Test
+    public void JpaEventBasicEntity() throws Exception {
+        Member member = new Member("memberA");
+        memberRepository.save(member); //@PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("memberB");
+
+        em.flush(); ////@PreUpdate
+        em.clear();
+
+        Member findMember = memberRepository.findById(member.getId()).get();
+        System.out.println("findMember.getCreateDate() = " + findMember.getCreateDate());
+        System.out.println("findMember.getUpdateDate() = " + findMember.getUpdateDate());
     }
 }
